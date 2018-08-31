44052c214713eb619f2af27d08ed521af2e706cf
setsunafseisei <380490126@qq.com>
2018-08-31 14:09:06 +0800

    first

---
+++ src/components/pos.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pos">
+      AwesomePos Demo
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pos',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
